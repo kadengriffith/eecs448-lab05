89ec8537701723d8e7ea6565a4a2312af54c0f8c
Kaden Griffith <kadengriffith@ku.edu>
2017-10-23 10:09:29 -0500

    Bug fix preventing posts to be viewed / deleted

--- exercise6/viewUserPosts.php
+++ exercise6/viewUserPosts.php
@@ -9,7 +9,7 @@
   $query = "SELECT * FROM Users WHERE username='" . $user . "'";
   $result = $connection->query($query);
   $row = $result->fetch_assoc();
-  $author = $row["user_id"];
+  $author = $row['user_id'];
   $result = $connection->query("SELECT content FROM Posts WHERE author_id='" . $author . "'");
   if($result->num_rows == 0) {
     echo "<h5>" . $user . " has no posts...</h5>";
@@ -17,7 +17,7 @@
   }else {
     echo "<h3>" . $user . "'s posts:</h3>";
     while($row = $result->fetch_assoc()) {
-      echo "<br>" . $row["content"] . "<br><br>";
+      echo "<br>" . $row['content'] . "<br><br>";
     }
     echo "<br><br><a style='text-decoration:none;color:#333;' href='../adminHome.html'>Back</a>";
   }
