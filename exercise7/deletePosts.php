1965d65c831c66d36bff14900fe6e4ccc36df22d
Kaden Griffith <kadengriffith@ku.edu>
2017-10-22 19:19:45 -0500

    Bug fix preventing posts to be viewed / deleted

--- exercise7/deletePosts.php
+++ exercise7/deletePosts.php
@@ -1,11 +1,11 @@
 <?php
-  $connection = new mysqli("mysql.eecs.ku.edu", "kgriffith", 'P@$$word.123', "kgriffith"); // Establish connection
-  if($connection->connect_errno) {
-    printf("Something went wrong: %s", $connection->connect_error);
+  $userPostsSub_connection = new mysqli("mysql.eecs.ku.edu", "kgriffith", 'P@$$word.123', "kgriffith"); // Establish connection
+  if($userPostsSub_connection->connect_errno) {
+    printf("Something went wrong: %s", $userPostsSub_connection->connect_error);
     exit();
   }
   echo "<html><body>";
-  $posts = $connection->query("SELECT * FROM Posts");
+  $posts = $userPostsSub_connection->query("SELECT * FROM Posts");
   $checked = false;
   while($row = $posts->fetch_assoc()) {
     if(isset($_POST[$row['post_id']])) {
@@ -16,13 +16,13 @@
     echo "<h5>Error: No post to delete</h5><br><br><a style='text-decoration:none;color:#333;' href='../adminHome.html'>Back</a>";
     exit();
   }
-  $posts = $connection->query("SELECT * FROM Posts");
+  $posts = $userPostsSub_connection->query("SELECT * FROM Posts");
   echo "<h5>Post(s) successfully deleted...</h5>";
   while($row = $posts->fetch_assoc()) {
     if(isset($_POST[$row['post_id']])) {
       echo $row['post_id'] . "<br>";
       $delete = "DELETE FROM Posts WHERE post_id='" . $row['post_id'] . "'";
-      $connection->query($delete);
+      $userPostsSub_connection->query($delete);
     }
   }
   echo "<br><br><a style='text-decoration:none;color:#333;' href='../adminHome.html'>Back</a>";
